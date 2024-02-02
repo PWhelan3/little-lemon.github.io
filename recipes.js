48fc4d45c695e88744d47d8e1af6b718d597acff
PWhelan3 <149814036+PWhelan3@users.noreply.github.com>
2024-02-02 23:41:16 +0000

    Add files via upload

---
+++ recipes.js
@@ -0,0 +1,29 @@
+const recipes = [
+  {
+    id: 1,
+    title: "Antipasto",
+    price: 14.99,
+    image: "https://popmenucloud.com/cdn-cgi/image/width%3D1200%2Cheight%3D1200%2Cfit%3Dscale-down%2Cformat%3Dauto%2Cquality%3D60/bhvlpyuw/4c34d8eb-e10e-40cc-999b-412ec0545344",
+    description:
+      "Our famous antipasto plate is perfect for 2, with fresh meditarranean elements that hit the spot!",
+  },
+  {
+    id: 2,
+    title: "Calamari",
+    price: 7.39,
+    image: "https://www.dailyvegan.recipes/wp-content/uploads/2020/10/EN_calamari-10.jpg",
+    description:
+      "Love seafood? Why not try our flavourful calamari cooked to order and freshly delivered everyday.",
+  },
+  {
+    id: 3,
+    title: "Chcolate Mousse",
+    price: 3.59,
+    image: "https://www.onceuponachef.com/images/2019/04/Chocolate-Mousse.jpg",
+    description:
+      "More decadant than you can imagine, our family secret recipe chocolate mousse is something out of heaven",
+  },
+
+];
+
+  export default recipes;
